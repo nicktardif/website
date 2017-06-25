1d1160b5d08aa294fc4fc7c77a259b50abae650c
Nick Tardif <nicktardif@gmail.com>
2017-06-25 13:06:29 -0400

    Update filters functionality + icons

--- js/main.js
+++ js/main.js
@@ -1,5 +1,43 @@
 filters = []
 
+/*
+ * Replace all SVG images with inline SVG
+ */
+function svgToInline() {
+  jQuery('img.svg').each(function(){
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function(data) {
+      // Get the SVG tag, ignore the rest
+      var $svg = jQuery(data).find('svg');
+
+      // Add replaced image's ID to the new SVG
+      if(typeof imgID !== 'undefined') {
+        $svg = $svg.attr('id', imgID);
+      }
+      // Add replaced image's classes to the new SVG
+      if(typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass+' replaced-svg');
+      }
+
+      // Remove any invalid XML tags as per http://validator.w3.org
+      $svg = $svg.removeAttr('xmlns:a');
+
+      // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+      if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+        $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+      }
+
+      // Replace image with new SVG
+      $img.replaceWith($svg);
+
+    }, 'xml');
+  });
+};
+
 function initPhotoSwipeFromDOM(gallerySelector) {
 
     // parse slide data (url, title, size ...) from DOM elements 
@@ -269,8 +307,9 @@ function addImages(gallerySelector) {
 window.onload = function() {
   gallerySelector = '.my-gallery';
   addImages(gallerySelector);
+  svgToInline();
 
-  $('#travel-filter').click(function() {
+  $(document).on('click','#travel-filter',function() {
     if ($(this).hasClass('active')) {
       index = filters.indexOf('travel');
       if(index > -1) {
@@ -285,7 +324,7 @@ window.onload = function() {
     addImages(gallerySelector);
   });
 
-  $('#food-filter').click(function() {
+  $(document).on('click','#food-filter',function() {
     if ($(this).hasClass('active')) {
       index = filters.indexOf('food');
       if(index > -1) {
@@ -296,6 +335,15 @@ window.onload = function() {
       filters.push('food');  
       $(this).addClass('active');
     }
+    $(gallerySelector).empty();
+    addImages(gallerySelector);
+  });
+
+  $(document).on('click','#reset-filter',function() {
+    $(document).find('#travel-filter').removeClass('active')
+    $(document).find('#food-filter').removeClass('active')
+    filters = []
+
     $(gallerySelector).empty();
     addImages(gallerySelector);
   });
