019f68f092730340266172402540fd7e42beefe6
Nick Tardif <nicktardif@gmail.com>
2018-01-05 07:18:23 -0500

    Updating the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash 
-rsync -rv --exclude 'input_images' --exclude '.git' --exclude 'node_modules' . root@138.197.91.238:/root
+rsync -rv --exclude 'input_images' --exclude '.git' --exclude 'node_modules' . root@138.197.91.238:/var/www/nicktardif/
