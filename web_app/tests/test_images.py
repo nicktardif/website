b8c9f9a33b7964b6d764b2f2ed82d6afd2eebed2
Nick Tardif <nicktardif@gmail.com>
2020-05-17 15:07:55 -0700

    Add update functionality Add in validators for API calls

--- web_app/tests/test_images.py
+++ web_app/tests/test_images.py
@@ -8,6 +8,7 @@ from web_app.utilities.file_helper import get_full_path
 import datetime
 import os
 import unittest
+import pytz
 
 def populate_database(db):
     sample_keyword = Keyword('landscape')
@@ -58,78 +59,110 @@ class GetAllImagesTest(SampleTestCase):
 class CreateImageTest(SampleTestCase):
     def test_create_image(self):
         data = {'image_name': 'test.jpg', 'image_data': image_to_base64('/tmp/images/test.jpg')}
-        response = self.client.post('/api/v1/images', data=data)
-        print(response.json)
+        response = self.client.post('/api/v1/images', json=data)
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
 
         expected_image_id = 1
         expected_response = Image.query.get(expected_image_id)
 
         self.assertEquals(response.json, jsonify(expected_response).json)
-        print(response.json)
 
     def test_create_image_missing_name(self):
         data = {'image_data': 'test.jpg'}
-        response = self.client.post('/api/v1/images', data=data)
+        response = self.client.post('/api/v1/images', json=data)
 
-        expected_response = {'error': 'Did not supply image_name in the data field'}
+        expected_response = {'error': 'Did not supply image_name in the JSON payload'}
         self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEquals(response.json, jsonify(expected_response).json)
         # TODO: Need to check for the caption, location, date, and keyword values
 
     def test_create_image_missing_data(self):
         data = {'image_name': 'test.jpg'}
-        response = self.client.post('/api/v1/images', data=data)
+        response = self.client.post('/api/v1/images', json=data)
 
-        expected_response = {'error': 'Did not supply image_data in the data field'}
+        expected_response = {'error': 'Did not supply image_data in the JSON payload'}
         self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEquals(response.json, jsonify(expected_response).json)
 
-    def test_create_image_no_data_field(self):
+    def test_create_image_duplicate_filename(self):
+        populate_database(db)
+        data = {'image_name': 'yolo.jpg', 'image_data': image_to_base64('/tmp/images/test.jpg')}
+        response = self.client.post('/api/v1/images', json=data)
+
+        expected_response = {'error': 'Image with the name yolo.jpg already exists in the database'}
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEquals(response.json, jsonify(expected_response).json)
+
+    def test_create_image_no_json(self):
         response = self.client.post('/api/v1/images')
 
-        expected_response = {'error': 'Request did not include a data form, try again'}
+        expected_response = {'error': 'Request did not include a JSON payload, try again'}
         self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEquals(response.json, jsonify(expected_response).json)
 
-#class UpdateLocationTest(SampleTestCase):
-#    def test_update_location(self):
-#        populate_database(db)
-#
-#        location_id = 1
-#        data = {'name': 'Sunspear'}
-#        response = self.client.patch('/api/v1/locations/{}'.format(location_id), data=data)
-#
-#        location = Location.query.get(location_id)
-#
-#        self.assertEquals(response.status_code, status.HTTP_200_OK)
-#        self.assertEquals(response.json, jsonify(location).json)
-#
-#    def test_update_location_invalid_id(self):
-#        location_id = 1
-#        data = {'name': 'Sunspear'}
-#        response = self.client.patch('/api/v1/locations/{}'.format(location_id), data=data)
-#
-#        expected_response = {'error': 'Location with the ID {} does not exist in the database'.format(location_id)}
-#        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
-#        self.assertEquals(response.json, jsonify(expected_response).json)
-#
-#    def test_update_location_no_name(self):
-#        location_id = 1
-#        data = {'library': 'Oldtown'}
-#        response = self.client.patch('/api/v1/locations/{}'.format(location_id), data=data)
-#
-#        expected_response = {'error': 'Did not supply name in the data field'}
-#        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
-#        self.assertEquals(response.json, jsonify(expected_response).json)
-#
-#    def test_update_location_no_data(self):
-#        location_id = 1
-#        response = self.client.patch('/api/v1/locations/{}'.format(location_id))
-#
-#        expected_response = {'error': 'Request did not include a data form, try again'}
-#        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
-#        self.assertEquals(response.json, jsonify(expected_response).json)
+class UpdateImageTest(SampleTestCase):
+    def test_update_image_caption(self):
+        populate_database(db)
+
+        image_id = 1
+        new_caption = 'yolo test'
+        data = {'caption': new_caption}
+        expected_image = Image.query.get(image_id)
+        expected_image.caption = new_caption
+
+        response = self.client.patch('/api/v1/images/{}'.format(image_id), json=data)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.json, jsonify(expected_image).json)
+
+        expected_image.delete()
+
+    def test_update_image_location(self):
+        populate_database(db)
+
+        image_id = 1
+        new_location = 'new place, FL'
+        data = {'location': new_location}
+        expected_image = Image.query.get(image_id)
+        expected_image.location = new_location
+
+        response = self.client.patch('/api/v1/images/{}'.format(image_id), json=data)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.json, jsonify(expected_image).json)
+
+        expected_image.delete()
+
+    def test_update_image_date(self):
+        populate_database(db)
+
+        image_id = 1
+        new_date = datetime.datetime.now().astimezone(pytz.utc)
+
+        data = {'date': new_date.isoformat()}
+        expected_image = Image.query.get(image_id)
+        expected_image.date = new_date
+
+        response = self.client.patch('/api/v1/images/{}'.format(image_id), json=data)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(response.json, jsonify(expected_image).json)
+
+        expected_image.delete()
+
+    def test_update_image_invalid_id(self):
+        image_id = 1
+        data = {'caption': 'bad test'}
+        response = self.client.patch('/api/v1/images/{}'.format(image_id), json=data)
+
+        expected_response = {'error': 'Image with the ID {} does not exist in the database'.format(image_id)}
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEquals(response.json, jsonify(expected_response).json)
+
+    def test_update_image_no_data(self):
+        image_id = 1
+        response = self.client.patch('/api/v1/images/{}'.format(image_id))
+
+        expected_response = {'error': 'Request did not include a JSON payload, try again'}
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEquals(response.json, jsonify(expected_response).json)
 
 class DeleteLocationTest(SampleTestCase):
     def test_delete_image(self):
--- web_app/utilities/__init__.py
+++ web_app/utilities/__init__.py
@@ -1,1 +1,3 @@
 from .json_encoder import CustomJSONEncoder
+from .validator import Validator
+from .validation import Validation
