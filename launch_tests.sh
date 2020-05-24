f6d459599d2ca8a4656e90c06868bf74b2f671cc
Nick Tardif <nicktardif@gmail.com>
2020-05-24 12:58:17 -0700

    Added test/production configuration options

--- launch_tests.sh
+++ launch_tests.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-pipenv run /bin/bash -c 'export FLASK_APP=web_app; flask db upgrade'
-pipenv run nose2
+pipenv run /bin/bash -c 'export FLASK_APP=web_app; export FLASK_CONFIG=testing; flask db upgrade; nose2'
--- web_app/__init__.py
+++ web_app/__init__.py
@@ -4,9 +4,6 @@ from web_app.make_database import db
 from flask_migrate import Migrate
 from web_app.utilities import CustomJSONEncoder
 from web_app.views import AlbumApiView, ImageApiView, PortfolioApiView
-from flasgger import Swagger
-
-swagger = Swagger(app, template=app.config['SWAGGER_TEMPLATE'])
 
 migrate = Migrate(app, db)
 app.json_encoder = CustomJSONEncoder
