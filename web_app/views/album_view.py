f40a54ba34adab59a80577e41fd826dc5c1cf29c
Nick Tardif <nicktardif@gmail.com>
2020-06-06 16:50:56 -0700

    Add ability for albums to add/remove portfolios

--- web_app/views/album_view.py
+++ web_app/views/album_view.py
@@ -1,12 +1,13 @@
 from web_app import app
-from web_app.models import Album, Image
+from web_app.models import Album, Image, Portfolio
 from flask import render_template
 
 class AlbumView():
     @app.route('/albums/<int:album_id>')
     def get_album(album_id):
         album = Album.query.get(album_id)
-        return render_template('album.html', album=album)
+        portfolios = Portfolio.query.all()
+        return render_template('album.html', album=album, portfolios=portfolios)
 
     @app.route('/albums')
     def get_all_albums():
