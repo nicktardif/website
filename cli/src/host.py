6a2b517409e404ed833fd5f8874d78ac52e79bf1
Nick Tardif <nicktardif@gmail.com>
2020-06-14 18:18:54 -0700

    Remove old codebase

--- cli/src/host.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import os
-
-def host_website(build_dir):
-    os.chdir(build_dir)
-    host_process = subprocess.Popen('python -m http.server 8000', shell=True)
-    host_process.wait()
