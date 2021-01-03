f14909fb72fb0eacf264e4908291cdde3af19270
Nick Tardif <nicktardif@gmail.com>
2021-01-03 12:19:18 -0800

    Update run script to handle concurrent requests

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ pipenv run /bin/bash -c "${SECRETS} export FLASK_APP=web_app; flask db upgrade"
 if [[ $1 = "DEBUG" ]]; then
 pipenv run /bin/bash -c "${SECRETS} export FLASK_ENV=development; export FLASK_DEBUG=1; export FLASK_APP=web_app:app; flask run -p 8000"
 else
-pipenv run /bin/bash -c "${SECRETS} export FLASK_DEBUG=1; gunicorn --bind unix:/tmp/nicktardif.sock --group www-data web_app:app -t 1800 --access-logfile -"
+pipenv run /bin/bash -c "${SECRETS} export FLASK_DEBUG=1; gunicorn -w 3 --threads 12 --bind unix:/tmp/nicktardif.sock --group www-data web_app:app -t 1800 --access-logfile -"
 fi
