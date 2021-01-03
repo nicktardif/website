08af2556363269df23ddc7d52094a5d2c1975115
Nick Tardif <nicktardif@gmail.com>
2021-01-03 19:45:08 +0000

    Final changes to deploy website with systemctl

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 mkdir -p $PWD/web_app/static/data/ # Create working folder for data
 
 SECRETS=$(cat secrets);
@@ -6,5 +8,5 @@ pipenv run /bin/bash -c "${SECRETS} export FLASK_APP=web_app; flask db upgrade"
 if [[ $1 = "DEBUG" ]]; then
 pipenv run /bin/bash -c "${SECRETS} export FLASK_ENV=development; export FLASK_DEBUG=1; export FLASK_APP=web_app:app; flask run -p 8000"
 else
-pipenv run /bin/bash -c "${SECRETS} export FLASK_DEBUG=1; gunicorn web_app:app"
+pipenv run /bin/bash -c "${SECRETS} export FLASK_DEBUG=1; gunicorn --bind unix:/tmp/nicktardif.sock --group www-data web_app:app -t 1800 --access-logfile -"
 fi
